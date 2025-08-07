106f33a7ca4fc5b8ccef5b951962c8dd411aa6a6
Sharath-holla <nssharath123@gmail.com>
2025-08-07 12:37:02 +0530

    Update foodsecurity.js

--- routes/foodsecurity.js
+++ routes/foodsecurity.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Technology = require("../models/foodSecurity"); // Match this with your model file name
+const Technology = require("../models/technology"); 
 const { isLoggedIn } = require('../middleware'); // Middleware for authentication (if used)
 
 // Utility function to validate ObjectId
